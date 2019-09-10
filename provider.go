a11fdb7e72cc19e2f021274e31bec0b699f56631
Meghan Denny <nektromc@gmail.com>
2019-09-10 02:04:31 -0400

    add AppConf struct

--- provider.go
+++ provider.go
@@ -10,6 +10,15 @@ type Provider struct {
 	NamePrefix   string `json:"name_prefix"`
 }
 
+type AppConf struct {
+	For    string `json:"for"`
+	ID     string `json:"id"`
+	Secret string `json:"secret"`
+	Extra1 string `json:"extra_1"`
+	Extra2 string `json:"extra_2"`
+	Extra3 string `json:"extra_3"`
+}
+
 var (
 	ProviderDiscord = Provider{
 		"discord",
