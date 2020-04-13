ce44de6665bd654dc853352713d6db4e84c884e9
Meghan Denny <nektromc@gmail.com>
2020-04-12 20:29:27 -0400

    simplify build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-init() {
-    go get -v -u github.com/rakyll/statik
-    $GOPATH/bin/statik -src="./www/"
-}
-
-init
+go get -v -u github.com/rakyll/statik
+$GOPATH/bin/statik -src="./www/"
